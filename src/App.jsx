2352e6c7681d09f545e7d185a31bbd7a219b0169
Suvash Joshi <suvash2077@gmail.com>
2023-12-23 16:21:54 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+  return (
+    <div>
+      <div>
+        <p>Thursday</p>
+        <p>07 December 2023</p>
+      </div>
+      <div>
+        <div>O</div>
+        <p>Gamcha, Budha-Nilkantha, Kathmandu, Nepal</p>
+        <div>
+          <img src="" />
+          <img src="" />
+          <img src="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
