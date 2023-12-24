3e1c433926a9fd7c1c10e9a8b0baa23b6eafb2a5
Suvash Joshi <suvash2077@gmail.com>
2023-12-24 21:42:04 +0545

    Added navbar background change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import Header from "./components/Header";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { useState } from "react";
 
 function App() {
+  const [navbar, setNavbar] = useState(false);
+
+  //Navbar scroll animation
+  const changeNavbarBackground = () => {
+    if (window.scrollY >= 56) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeNavbarBackground);
+
   return (
     <div className="relative">
-      <div className="fixed z-50 inset-x-0">
-        <Header />
+      <div
+        className={
+          navbar
+            ? "fixed z-50 inset-x-0 py-2 bg-yellow-600"
+            : "fixed z-50 inset-x-0 py-2"
+        }
+      >
         <Navbar />
       </div>
       <Homepage />
